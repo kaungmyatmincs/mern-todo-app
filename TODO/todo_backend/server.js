cc4e5fdfbbf8c4bcc95df8b48772e8f4a1de9671
kaungmyatmincreaterecord@gmail.com <your@email.com>
2026-03-13 14:36:59 +0700

    Run 11: Fix static path and connection logic in server.js

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -7,58 +7,65 @@ const TodoModel = require('./models/Todo');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "static/build")));
+
+// FIXED: Matches the 'static' folder created by your Jenkinsfile
+app.use(express.static(path.join(__dirname, "static")));
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TODO';
 const PORT = process.env.PORT || 5000;
 
-console.log('Connecting to MongoDB...',MONGODB_URI  );
-mongoose.connect(MONGODB_URI,
-    console.log('MongoDB connected')
-)
+console.log('Connecting to MongoDB...', MONGODB_URI);
 
-app.listen(PORT,
-    console.log('Server listening on port: 5000')
-)
+// FIXED: Corrected callback syntax
+mongoose.connect(MONGODB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
 
 app.post('/add', (req, res) => {
-  const { task } = req.body;
-  TodoModel.create({ task })
-      .then(result => res.json(result))
-      .catch(err => console.log(err));
-   
+    const { task } = req.body;
+    TodoModel.create({ task })
+        .then(result => res.json(result))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-app.get('/get',(req,res)=>{
-  TodoModel.find()
-  .then(result=> res.json(result))
-  .catch(err=>console.log(err));
+app.get('/get', (req, res) => {
+    TodoModel.find()
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json(err));
 });
-  
-app.put('/edit/:id',(req,res)=>{
-  const{id} = req.params;
-  TodoModel.findByIdAndUpdate(id,{done:true},{new:true})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- });
 
-app.put('/update/:id',(req,res)=>{
-  const{id} = req.params;
-  const{task} = req.body;
-  TodoModel.findByIdAndUpdate(id,{task:task})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- });
+app.put('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndUpdate(id, { done: true }, { new: true })
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json(err));
+});
 
-app.delete('/delete/:id',(req,res)=>{
-  const{id} = req.params;
-  TodoModel.findByIdAndDelete({_id:id})
-  .then(result=> res.json(result))
-  .catch(err=>res.json(err));
- }); 
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { task } = req.body;
+    TodoModel.findByIdAndUpdate(id, { task: task })
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json(err));
+});
 
- app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/build", "index.html"));
- });
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    TodoModel.findByIdAndDelete({ _id: id })
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json(err));
+});
 
-module.exports=app;
+// FIXED: Catch-all to serve index.html for React routing
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+module.exports = app;
